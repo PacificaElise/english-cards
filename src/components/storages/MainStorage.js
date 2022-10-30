4a2e599a64029c13d368c358c57df9d4b8db8ba3
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2022-10-30 23:53:16 +0300

    add runInAction

--- src/components/storages/MainStorage.js
+++ src/components/storages/MainStorage.js
@@ -1,4 +1,4 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 
 class MainStorage {
   list = [];
@@ -10,8 +10,9 @@ class MainStorage {
   }
 
   async getWords() {
-    this.isLoading = true;
-    fetch('http://itgirlschool.justmakeit.ru/api/words')
+    runInAction (() => {
+      this.isLoading = true;
+      fetch('http://itgirlschool.justmakeit.ru/api/words')
         .then(res => res.json())
         .then((json) => {
           this.list = json;
@@ -20,24 +21,27 @@ class MainStorage {
           this.error = true;
         }).finally(() => {
           this.isLoading = false;
-        })
+        });
+      });
   }
 
-  deleteWord = async (id) => {
-    let isDelete = window.confirm("Вы действительно хотите удалить это слово?");
-    if (isDelete) {
-      try {
-        const res = await fetch(`http://itgirlschool.justmakeit.ru/api/words/${id}/delete`, {
-          method: 'POST',
-        });
-        if(res.ok) {
-          let newList = [...this.list].filter(item => item.id!==id);
+  deleteWord = (id) => {
+    runInAction (async () => {
+      let isDelete = window.confirm("Вы действительно хотите удалить это слово?");
+      if (isDelete) {
+        try {
+          const res = await fetch(`http://itgirlschool.justmakeit.ru/api/words/${id}/delete`, {
+            method: 'POST',
+          });
+          if(res.ok) {
+            let newList = [...this.list].filter(item => item.id!==id);
             this.list = newList;
+          };
+        } catch(e) {
+          alert(`Ошибка соединения с сервером. ${e}`);
         };
-      } catch(e) {
-        alert(`Ошибка соединения с сервером. ${e}`);
-      };
-    }
+      }
+    });
   }
 }
 
